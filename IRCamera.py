9fb2ed18a4d45d6fc685e81490342c4825b90ec9
Kevin Johnson <joh06018@byui.edu>
2016-06-02 16:59:07 -0600

    Made the project PEP8-compliant

--- IRCamera.py
+++ IRCamera.py
@@ -1,14 +1,16 @@
-
+"""This is the actual module for using the IR camera with the Pi."""
 
 import pigpio
 from time import sleep
 
+
 class IRCamera:
-    """"Class for handling sensor communication"""
+    """Class for handling sensor communication."""
 
     def __init__(self, i2c_bus=1, sensor_address=0x58):
         """
-        Initialize the IR Camera
+        Initialize the IR Camera.
+
         Parameters:
             i2c_bus: The number of the i2c bus (default 1)
             sensor_address: the i2c port for the camera (default 0x58)
@@ -25,8 +27,11 @@ class IRCamera:
         self.__initialize_sensor()
 
     def __initialize_sensor(self):
-        """Send the commands to the IR camera to ready it for use"""
-        init_commands = [0x30, 0x01, 0x30, 0x08, 0x06, 0x90, 0x08, 0xC0, 0x1A, 0x40, 0x33, 0x33]
+        """Send the commands to the IR camera to ready it for use."""
+        init_commands = {
+            [0x30, 0x01, 0x30, 0x08, 0x06,
+             0x90, 0x08, 0xC0, 0x1A, 0x40, 0x33, 0x33]
+        }
         for i, j in zip(init_commands[0::2], init_commands[1::2]):
             self.pi.i2c_write_byte_data(self.device, i, j)
             sleep(0.01)
@@ -34,8 +39,10 @@ class IRCamera:
     def get_positions(self):
         """
         Ask the sensor for positions of IR emitters.
+
         Returns:
-            List of found IR emitters in [x, y] format. Returns an empty list if none are found.
+            List of found IR emitters in [x, y] format.
+            Returns an empty list if none are found.
         """
         self.pi.i2c_write_byte(self.device, 0x36)
         data = self.pi.i2c_read_i2c_block_data(self.device, 0x36, 16)
@@ -49,11 +56,12 @@ class IRCamera:
             x = data[1][j] + ((data[1][j+2] & 0x30) << 4)
             y = data[1][j+1] + ((data[1][j+2] & 0xC0) << 2)
             i += 1
-            if x != 1023 or y != 1023:  # 1023,1023 is the designated value for NOT FOUND
+            if x != 1023 or y != 1023:  # 1023,1023 means NOT FOUND
                 positions.append([x, y])  # append to list
 
         # Note that if nothing was found, positions will be an empty list
         return positions
 
     def close_connection(self):
+        """Close the I2C connection."""
         self.pi.close(self.device)
--- PositionTracker.py
+++ PositionTracker.py
@@ -1,11 +1,15 @@
+"""This module tracks your location."""
+
 from IRCamera import IRCamera
 from time import sleep
 
 
 # IMPORTANT NOTES
 # This tracker requires the data to be set in the SETTINGS section.
-# Hardware-wise, this tracker requires an isosceles triangle (NOT EQUILATERAL) of IR LEDs to be mounted above the camera
-# Ideally, the triangle will point north for ease of accuracy checking using a magnetometer (optional)
+# Hardware-wise, this tracker requires an isosceles triangle (NOT EQUILATERAL)
+# of IR LEDs to be mounted above the camera
+# Ideally, the triangle will point north for ease of accuracy checking using a
+# magnetometer (optional)
 
 # SETTINGS
 i2c_bus = 1  # i2c bus number (either 0 or 1)
@@ -13,15 +17,16 @@ ir_camera_port = 0x58
 ir_emitter_pattern_width = 3  # inches
 ir_emitter_pattern_height = 6  # inches
 ceiling_height = 96  # inches
-has_magnetometer = False  # enables error correction if the magnetometer is available
+has_magnetometer = False  # enables error correction using magnetometer
 
 
 # make your camera object
 camera = IRCamera(i2c_bus, ir_camera_port)
 
-# calculate the size of the square you can travel in (so you can report location accurately)
-# We factor in a 10% overlap in the hopes of making absolutely sure we can't travel too far
-
+# calculate the size of the square you can travel in (so you can report your
+# location accurately)
+# We factor in a 10% overlap in the hopes of making absolutely sure we can't
+# travel too far and lose sight of the sensor
 
 # Testing
 while True:
